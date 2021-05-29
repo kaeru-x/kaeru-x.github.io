243d87be619619127b33c0ade4d5678fb652a977
Haruka Sasaki <sasaki@hakoreco.com>
2021-05-29 09:11:55 +0900

    add: SexyVoiceButton

---
+++ SexyVoiceButton/index.js
@@ -0,0 +1,16 @@
+function soundKTY(){
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( 'KaxyThankYou' ).play() ;
+}
+
+function soundKR(){
+	document.getElementById( 'KaxyRose' ).play() ;
+}
+
+function soundKZ(){
+	document.getElementById( 'KaxyZone' ).play() ;
+}
+
+function soundSorry(){
+	document.getElementById( 'Sorry' ).play() ;
+}
