26273c4db630040eb8b9635fe7313b479f2df09b
Haruka Sasaki <sasaki@hakoreco.com>
2021-05-29 10:59:12 +0900

    add: SJVoiceButton

---
+++ SJVoiceButton/index.js
@@ -0,0 +1,16 @@
+function soundSN(){
+	// Serenadeの音声ファイルを再生[play()]する
+	document.getElementById( 'Serenade' ).play() ;
+}
+
+function soundKiyowo(){
+	document.getElementById( 'Kiyowo' ).play() ;
+}
+
+function soundSH(){
+	document.getElementById( 'Saranheyo' ).play() ;
+}
+
+function soundMuse(){
+	document.getElementById( 'Muse' ).play() ;
+}
